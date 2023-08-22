2086d1b3ed7b42b18b35a80726e86cee2c9d5340
albinsonarias <albinsonarias@gmail.com>
2023-08-22 10:48:57 -0400

    fix: :lipstick: Made lock orientation

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -28,6 +29,10 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   Bloc.observer = const AppBlocObserver();
 
   // Add cross-flavor configuration here
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   runApp(await builder());
 }
